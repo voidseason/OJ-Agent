387c794e120b2e53e9eff4e90d17fb02771f940b
Season <3047263011@qq.com>
2026-07-11 17:23:40 +0800

    在grpc_server创建了ChatServiceImpl模块文件，目的是实现protoc生成的服务接口编写

---
+++ grpc_server/include/ChatServiceImpl.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace OJ_Agent
+{
+    class ChatServiceImpl : public ChatService::Service  // protoc 生成的接口
+    {
+        public:
+        explicit ChatServiceImpl
+    }
+
+}
